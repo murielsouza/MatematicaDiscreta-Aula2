74e9e44ac84859357257e8d47993f243c030ab75
Muriel Souza <muriel-souza@live.com>
2019-02-13 00:22:54 -0300

    add de listas

---
+++ listas.py
@@ -0,0 +1,24 @@
+a = [1, 2, 3, 4]
+b = [1, 2, 3, 4]
+x = 1 #valor para verificar pertinencia
+
+#união
+print (a + b)
+
+#pertinencia
+if x in a:
+    print(True)
+else:
+    print(False)
+
+
+#igualdade
+if a == b:
+    print(True)
+else:
+    print(False)
+
+
+
+
+
